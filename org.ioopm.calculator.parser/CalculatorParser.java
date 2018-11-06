613353fc8cf6c0daa1ebe0fa6976c900491d7b77
brr3 <brunorees@yahoo.com>
2018-11-06 13:55:05 +0100

    copied code from assignment page, does not yet compile

--- org.ioopm.calculator.parser/CalculatorParser.java
+++ org.ioopm.calculator.parser/CalculatorParser.java
@@ -1,1 +1,31 @@
 package org.ioopm.calculator.parser;
+
+import java.io.StreamTokenizer;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class CalculatorParser {
+  private BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+  private final StreamTokenizer st = new StreamTokenizer(r);
+
+  public CalculatorParser() {
+    /// We want to treat - and end of line as an ordinary tokens
+    this.st.ordinaryChar('-'); /// parse object-oriented as "object" - "oriented" :)
+    this.st.eolIsSignificant(true);
+  }
+
+  public SymbolicExpression expression() {
+   SymbolicExpression result = term();
+   while (st.ttype == '+' || st.ttype == '-') {
+      int operation = st.ttype;
+      st.nextToken();
+      if (operation == '+') {
+          result = new SymbolicExpression("Addition", result, term()); // TODO: Update when AST is done
+      } else {
+          result = new SymbolicExpression("Subtraction", result, term()); // TODO: Update when AST is done
+      }
+   }
+   return result;
+ }
+}
---
+++ org.ioopm.calculator.parser/SyntaxErrorException.java
@@ -0,0 +1,8 @@
+public class SyntaxErrorException extends RuntimeException {
+    public SyntaxErrorException() {
+        super();
+    }
+    public SyntaxErrorException(String msg) {
+        super(msg);
+    }
+}
