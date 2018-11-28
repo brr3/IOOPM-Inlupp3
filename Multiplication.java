30ccd59d2e2b2bfa7073df213a060e16f5442558
brr3 <brunorees@yahoo.com>
2018-11-28 19:31:03 +0100

    division and multiplication eval updated

--- Multiplication.java
+++ Multiplication.java
@@ -18,7 +18,7 @@ public class Multiplication extends Binary {
         if (lhs.isConstant() && rhs.isConstant()) {
             return new Constant(lhs.getValue() * rhs.getValue());
         } else if (lhs.isConstant()) {
-            return new Multiplication(new Constant(lhs.getValue()), new Variable(rhs.toString()));
+            return new Multiplication(new Constant(lhs.getValue()), new Variable("(" + rhs.toString() + ")"));
         } else {
             return new Multiplication(new Variable("(" + lhs.toString() + ")"), new Constant(rhs.getValue()));
         }
